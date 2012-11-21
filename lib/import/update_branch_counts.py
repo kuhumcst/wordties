dc809019c7cd40aceee5c324fe8033afd2407ed1
Mitchell Seaton <seaton@hum.ku.dk>
2012-11-21 12:08:36 +0100

    Updated branch count python script to handle unknown top node.

--- lib/import/update_branch_counts.py
+++ lib/import/update_branch_counts.py
@@ -1,36 +1,22 @@
 import psycopg2
 import networkx as nx
-#from networkx.traversal import dfs_postorder_nodes
+import sys
 
 conn = psycopg2.connect("dbname=andreord user=andreord")
 cur = conn.cursor()
 
+# Uncomment if want to use db relations table in replacement of file input
 #cur.execute("SELECT syn_set_id, target_syn_set_id " +
 #             "FROM relations r " +
 #             "JOIN relation_types rt ON r.relation_type_id = rt.id "
 #             "WHERE rt.name = %s ", ('has_hyperonym',))
-#top_node = 20633000L
+#resultList = cur.fetchall()
+#
 
 # Set imported relations file (csv)
-#f=open('/home/seaton/git/andreord-public/lib/import/dan_net_data/relations.csv')  # DanNet relations
-f=open('/home/seaton/git/andreord-public/lib/import/pwn_data/EstWN/EstWNAndreOrd/relations.csv')
-
-# DELETEd this:
-#  2819377 |              258 |                    |   48335000 |           |                     |          15592000
-# 2617606 |              279 |                    |   15592000 |           | Inherited from synset with id 15567 ({brugsgenstand_1; brugsting_1}) |          48335000
-
+f=open('/home/seaton/git/andreord-public/lib/import/dan_net_data/relations.csv')
 
 G=nx.DiGraph()
-#for record in cur:
-#    src, target = record[0], record[1]
-#    G.add_edge(target, src)
-
-#for node in nx.dfs_postorder(G, 20633000L):
-#    counts = [
-#        G.node[succ]['branch_count']
-#        for succ in G.successors(node)]
-#    G.node[node]['branch_count'] = sum(counts) + 1
-
 
 for line in f.readlines():
     record = line.split("@")
@@ -56,29 +42,40 @@ for line in f.readlines():
 #G.remove_nodes_from(dangling_nodes)
 ###################################################
 
+
 # Note: Update reference to top-most node before using ie {DN:TOP}
-top_node = "20633"
-synset_factor = 1000;
+#top_node = "20633" # set if top node exists
+synset_factor = 1000 # db factoring
+top_node = None #str(20633)
 
-for node in nx.traversal.dfs_postorder_nodes(G, top_node):
-    counts = [
-        G.node[succ]['branch_count']
-        for succ in G.successors(node)]
-    G.node[node]['branch_count'] = sum(counts) + 1
+def traverse(top_node):
+	try:
+		for node in nx.traversal.dfs_postorder_nodes(G, top_node):
+		    counts = [G.node[succ]['branch_count'] for succ in G.successors(node)] 
+		    G.node[node]['branch_count'] = sum(counts) + 1
+	except:
+		print "Error occurred:", sys.exc_info()[0]
+		# raise
 
-for node in G.nodes():
-    db_node = int(node) * synset_factor # Applied synset factoring
-    if 'branch_count' not in G.node[node]:
-        print "{} is missing branch_count".format(node)
-    else:
-        sql = ("UPDATE syn_sets SET hyponym_count = %d WHERE id = %d" % (G.node[node]['branch_count'], db_node))
+def update():
+	cur = conn.cursor()
+	for node in G.nodes():
+		db_node = int(node) * synset_factor # Applied synset factoring
+		if 'branch_count' not in G.node[node]:
+			print "{} is missing branch_count".format(node)
+		else:
+			sql = ("UPDATE syn_sets SET hyponym_count = %d WHERE id = %d" % (G.node[node]['branch_count'], db_node))
+			if not cur.execute(sql):
+			    print cur.statusmessage
+			    conn.commit()
 
-#    print sql
-        if not cur.execute(sql):
-            print cur.statusmessage
+if top_node == None:
+	for target in nx.topological_sort(G):
+	    traverse(str(target))
+	
+	update()
+else:
+	traverse(top_node)
+	update()
 
-conn.commit()
 
-#degree_2 = [node for node in G if G.degree(node) > 1]
-#for component in nx.connected_components(nx.Graph(G)):
-    #print component
